d88440fd2d867eee5c4efcf5c5b2ee7cc80073a3
Krampus101 <unstoppabletrio101@gmail.com>
2025-10-08 16:23:05 -0700

    Add files

---
+++ week-2/practice1.rs
@@ -0,0 +1,12 @@
+fn main () {
+	printin!("Welcome to COS 101!")
+
+	printin!("The course learning outcome is as follows:")
+
+	printin!("1. Distinguish between computer and computer programming,n
+	2. Develop some techniques in computer science,/n
+	3. Understand the different areas of study in computer science,/n
+	4. Conversant with applications of computer science,/n
+	6. Conversant with computer programming concepts");
+
+}
---
+++ week-2/practice_2.rs
@@ -0,0 +1,6 @@
+fn main() {
+	printin!();  // prints just a
+	printin!("hello ");//prints
+	printin!("format{} arguments", "some"); //prints format some arguments
+	printin!("My name is {}. I am the class rep of stream 1", "Dagogo William-Jumbo");
+}
