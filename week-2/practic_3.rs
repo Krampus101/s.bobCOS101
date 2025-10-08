699231bdb66c0ce83dbc6ef4c978a39500f417a3
Krampus101 <unstoppabletrio101@gmail.com>
2025-10-08 16:33:30 -0700

    Add files

---
+++ week-2/practic_3.rs
@@ -0,0 +1,5 @@
+fn main() {
+	Let fees = 25000;
+	Let salary:f64 = 35_000.00
+	printin!("fees is {} and salary is {}",fees,salary);
+}
---
+++ week-2/practice_4.rs
@@ -0,0 +1,11 @@
+fn main() {
+	Let p:f64 = 1000.0;
+	Let r:f64 = 1.0;
+	Let t:f64 = 2.0;
+
+	// simple interest
+	Let a = p * ( 1.0 + (r/ 100.0))* t;
+	printin!("Amount is {}", a)
+	Let si = a - p;
+	printin!("	Simple Interest is {}", si);
+}
